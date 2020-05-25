b1218301b14ae2d958c9cf34072e7ead79ece742
russ3llwong <russellwong97@hotmail.com>
2020-05-24 19:00:36 -0700

    Added scribbleArea and basic menu actions

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,82 @@
+#include <QtWidgets>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "scribblearea.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    scribbleArea = new ScribbleArea;
+    setCentralWidget(scribbleArea);
+    createActions();
+    createMenus();
+    setWindowTitle(tr("Scribble"));
+    resize(500, 500);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    if(maybeSave()){
+        event->accept();
+    } else {
+      event->ignore();
+    }
+}
+
+void MainWindow::open()
+{
+    if(maybeSave()){
+        QString fileName = QFileDialog::getOpenFileName(this,
+                                                        tr("Open File"),
+                                                        QDir::currentPath());
+        if(!fileName.isEmpty()){
+            scribbleArea->openImage((fileName));
+        }
+
+    }
+}
+
+void MainWindow::save()
+{
+    QAction *action = qobject_cast<QAction *>(sender()); // action user is gonna click
+    QByteArray fileFormat = action->data().toByteArray();
+    saveFile(fileFormat);
+}
+
+void MainWindow::penColor()
+{
+    QColor newColor = QColorDialog::getColor(scribbleArea->penColor());
+    if(newColor.isValid()){
+        scribbleArea->setPenColor(newColor);
+    }
+}
+
+void MainWindow::penWidth()
+{
+    bool ok;
+    int newWidth = QInputDialog::getInt(this,
+                                        tr("Scribble"),
+                                        tr("Select pen width: "),
+                                        scribbleArea->penWidth(),
+                                        1, 50, 1, &ok);
+    if(ok){
+        scribbleArea->setPenWidth(newWidth);
+    }
+}
+
+void MainWindow::about()
+{
+    QMessageBox::about(this,
+                       tr("About Scribble"),
+                       tr("<p>The <b>Scribble</b> is awesome.</p>"));
+}
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QList>
+
+class ScribbleArea;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,36 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void closeEvent(QCloseEvent *event) override;
+
+private slots:
+    void open();
+    void save();
+    void penColor();
+    void penWidth();
+    void about();
+
 private:
-    Ui::MainWindow *ui;
+    void createActions();
+    void createMenus();
+    bool maybeSave();
+    bool saveFile(const QByteArray &fileFormat);
+    ScribbleArea *scribbleArea;
+    QMenu *saveAsMenu;
+    QMenu *fileMenu;
+    QMenu *optionMenu;
+    QMenu *helpMenu;
+    QAction *openAct;
+    QList<QAction *> saveAsActs;
+    QAction *penColorAct;
+    QAction *penWidthAct;
+    QAction *printAct;
+    QAction *clearScreenAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
+
+
+;    Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
